09cc2ffe8e4dff393b0217b09e34ace0e54fe8f9
Elvisley Araujo <elvisley.araujo@armateus.com.br>
2020-05-21 18:29:42 -0300

    ajuste de nomenclaturas

--- src/main/java/br/com/ithappens/controladoria/mapper/postgresql/LoteSinteticoSerieMapper.java
+++ src/main/java/br/com/ithappens/controladoria/mapper/postgresql/LoteSinteticoSerieMapper.java
@@ -3,13 +3,19 @@ package br.com.ithappens.controladoria.mapper.postgresql;
 import br.com.ithappens.controladoria.model.LoteSinteticoSerie;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 @Mapper
 public interface LoteSinteticoSerieMapper {
+
+    List<LoteSinteticoSerie> recuperarSinteticoSerie(@Param("codigoFilial") String codigoFilial,
+                                                     @Param("dataMovimento")LocalDate dataMovimento,
+                                                     @Param("idIntegrador") int idIntegrador);
 
     Boolean insertLoteSinteticoSerie(@Param("loteLst") List<LoteSinteticoSerie> loteLst);
 
